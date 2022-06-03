9d0bff19933fb88f975bd856b3c8b6c3ac019327
Николай <kolia047@mail.ru>
2022-06-03 21:08:40 +0600

    9. toString [#310018]

---
+++ src/main/java/ru/job4j/tracker/StarUI.java
@@ -0,0 +1,9 @@
+package ru.job4j.tracker;
+
+public class StarUI {
+
+    public static void main(String[] args) {
+        Item item = new Item("Firs");
+        System.out.println(item);
+    }
+}
