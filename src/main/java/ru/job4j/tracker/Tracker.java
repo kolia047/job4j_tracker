82fe070f84b2ac80823f930d6b99b56fc140a398
Николай <kolia047@mail.ru>
2022-06-03 16:11:59 +0600

    6. Метод замены заявки. Tracker.replace [#211748 #308504]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -49,8 +49,11 @@ public class Tracker {
 
     public boolean replace(int id, Item item) {
         int index = indexOf(id);
-        item.setId(id);
-        items[index] = item;
-        return true;
+        if (index != -1) {
+            item.setId(id);
+            items[index] = item;
+            return true;
+        }
+        return false;
     }
 }
