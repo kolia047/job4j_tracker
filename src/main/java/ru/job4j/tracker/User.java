9c5af5a2eade8af1147e78b94c24999ba53a6e6c
Николай <kolia047@mail.ru>
2022-06-03 21:12:26 +0600

    9. toString [#310018 #308707]

--- src/main/java/ru/job4j/tracker/User.java
+++ src/main/java/ru/job4j/tracker/User.java
@@ -26,11 +26,13 @@ public class User {
 
     @Override
     public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", username='" + username + '\'' +
-                ", password='" + password + '\'' +
-                '}';
+        return "User{"
+                + "id=" + id
+                + ", username='"
+                + username + '\''
+                + ", password='"
+                + password + '\''
+                + '}';
     }
 }
 
