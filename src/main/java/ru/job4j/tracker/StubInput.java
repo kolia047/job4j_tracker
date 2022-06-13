312f8d40bf6c87415cf163bf78151294c604d215
Николай <kolia047@mail.ru>
2022-06-13 15:10:08 +0600

    5. Input и полиморфизм. [#181094]

--- src/main/java/ru/job4j/tracker/StubInput.java
+++ src/main/java/ru/job4j/tracker/StubInput.java
@@ -1,6 +1,12 @@
 package ru.job4j.tracker;
 
-public class StubInput implements  Input {
+public class StubInput implements Input {
+    private String[] answers;
+    private int position = 0;
+
+    public StubInput(String[] answers) {
+        this.answers = answers;
+    }
 
     @Override
     public String askStr(String question) {
